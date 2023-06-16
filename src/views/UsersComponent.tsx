0fbcf07cfca65f0af4849ffc2d35ef55b33064ee
ardaalkan <ardaaa.alk@gmail.com>
2023-06-17 00:00:35 +0300

    added edit funcs to table

--- src/views/UsersComponent.tsx
+++ src/views/UsersComponent.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Row, Col, Table, Modal } from "antd";
+import { Row, Col, Table, Modal, Input, Button } from "antd";
 import { useQuery } from "react-query";
 import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
 
@@ -25,6 +25,8 @@ const UsersComponent: React.FC = () => {
   } = useQuery<UserData[]>("userData", fetchUsers, { staleTime: 50000 });
 
   const [userData, setUserData] = useState<UserData[]>([]);
+  const [show, setShow] = useState<boolean>(false);
+  const [editUser, setEditUser] = useState<UserData | null>(null);
 
   useEffect(() => {
     if (fetchedData) {
@@ -32,41 +34,8 @@ const UsersComponent: React.FC = () => {
     }
   }, [fetchedData]);
 
-  const columns = [
-    {
-      title: "ID",
-      dataIndex: "id",
-    },
-    {
-      title: "Name",
-      dataIndex: "name",
-    },
-    {
-      title: "Username",
-      dataIndex: "username",
-    },
-    {
-      title: "Action",
-      dataIndex: "action",
-      render: (_, record: UserData) => {
-        return (
-          <>
-            <DeleteOutlined
-              style={{ color: "red", margin: "5px" }}
-              onClick={() => Delete(record)}
-            />
-            {/* <EditOutlined
-              style={{ color: "blue", margin: "5px" }}
-              onClick={() => {}}
-            /> */}
-          </>
-        );
-      },
-    },
-  ];
-
   const Delete = (record: UserData) => {
-    console.log("delete funcs")
+    console.log("delete funcs");
     Modal.confirm({
       title: "Are you sure you want to delete this?",
       onOk: () => {
@@ -76,6 +45,55 @@ const UsersComponent: React.FC = () => {
       },
     });
   };
+
+  const Edit = (record: UserData) => {
+    console.log("edit funcs");
+    setEditUser({ ...record });
+    setShow(true);
+  };
+
+  const resetEditing = () => {
+    setEditUser(null);
+    setShow(false);
+  };
+
+  const columns = [
+    {
+      key: "id",
+      title: "ID",
+      dataIndex: "id",
+    },
+    {
+      key: "name",
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      key: "username",
+      title: "Username",
+      dataIndex: "username",
+    },
+    {
+      key: "action",
+      title: "Action",
+      dataIndex: "action",
+
+      render: (_, record: UserData) => {
+        return (
+          <>
+            <DeleteOutlined
+              style={{ color: "red", margin: "5px" }}
+              onClick={() => Delete(record)}
+            />
+            <EditOutlined
+              style={{ color: "blue", margin: "5px" }}
+              onClick={() => Edit(record)}
+            />
+          </>
+        );
+      },
+    },
+  ];
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -88,16 +106,64 @@ const UsersComponent: React.FC = () => {
   if (userData) {
     console.log(userData);
   }
+
   return (
     <div className="p-6">
       <Row gutter={1} style={{ marginTop: 10 }}>
         <Col span={18}>
+          <Button type="primary">Add New User</Button>
           <Table
             bordered
             dataSource={userData}
             columns={columns}
             pagination={{ pageSize: 10 }}
+            rowKey={(record) => record.id.toString()}
           ></Table>
+          <Modal
+            title="Edit User"
+            visible={show}
+            okText="Save"
+            onCancel={() => {
+              resetEditing();
+            }}
+            onOk={() => {
+              setUserData((prevData: UserData[]) => {
+                return prevData.map((user) => {
+                  if (user.id === editUser?.id) {
+                    return editUser;
+                  } else {
+                    return user;
+                  }
+                });
+              });
+              resetEditing();
+            }}
+          >
+            <Input
+              style={{ margin: "10px" }}
+              value={editUser?.name}
+              onChange={(e) => {
+                setEditUser((prevUser) => {
+                  if (prevUser) {
+                    return { ...prevUser, name: e.target.value };
+                  }
+                  return prevUser;
+                });
+              }}
+            />
+            <Input
+              style={{ margin: "10px" }}
+              value={editUser?.username}
+              onChange={(e) => {
+                setEditUser((prevUser) => {
+                  if (prevUser) {
+                    return { ...prevUser, username: e.target.value };
+                  }
+                  return prevUser;
+                });
+              }}
+            />
+          </Modal>
         </Col>
       </Row>
     </div>
